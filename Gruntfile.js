6ef511640dc8dbd2b1d8088afb250a89cdb3174a
lindamariemummy <lindamariemummy@gmail.com>
2014-11-16 14:29:50 -0800

    initial configuration

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        node: true
+      },
+      //src: ['models/**/*.js', 'server.js', 'routes/**/*.js']
+    },
+
+    jscs: {
+      //src: ['lib/*.js', 'routes/*.js', 'server.js', 'routes/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
+    simplemocha: {
+      //src: ['test/api/**/*.js']
+    }
+  });
+
+  grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
+  grunt.registerTask('default', ['test']);
+};
