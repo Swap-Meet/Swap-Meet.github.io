a144007c5b80e93c6a20c3249c2d6a5c4e149f04
lindamariemummy <lindamariemummy@gmail.com>
2014-11-18 14:26:25 -0800

    remove unnecessary error message

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -40,7 +40,7 @@ module.exports = function(app, auth) {
       console.log("body", data.body);
       total = data.length;
       passback = data.slice(start, Math.min(data.length+1,start+10))
-      if (!passback[0]) return res.status(200).json({error: 5});
+      //if (!passback[0]) return res.status(200).json({error: 5});
       res.status(200).json({"count": total,
         "items_left": Math.max(0,total - start - 10),
         "games":passback});
