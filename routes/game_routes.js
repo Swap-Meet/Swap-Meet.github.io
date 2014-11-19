4d68d88f6db5b4080863ec23ac8d4d2ab055d8a4
lindamariemummy <lindamariemummy@gmail.com>
2014-11-18 18:03:25 -0800

    fix hasgames/wantsgames POST req

--- routes/game_routes.js
+++ routes/game_routes.js
@@ -5,15 +5,16 @@ var User = require('../models/user');
 var plat, searchTerms, zipCode, expression, search, searchJSON;
 
 module.exports = function(app, auth) {
-  //return first 10 games in db (change this to random later)
+
+  //search all available games
   app.get('/api/wantsgames', auth, function(req, res) {
     var total, plat, searchTerms, passback, zipCode, expression, search, start = 0, totalLeft, searchJSON = {};
-    console.log(req);
+    //console.log(req);
     //read in params into object
     console.log("req.query", req.query);
 
     if (req.query.hasOwnProperty('q')) {
-      console.log(req.query.q);
+      //console.log(req.query.q);
       searchTerms = req.query.q.split('%20');
       for (var i = 0; i < searchTerms.length; i++) {
         searchTerms[i] = "(?=.*" + searchTerms[i] + ")";
@@ -33,9 +34,6 @@ module.exports = function(app, auth) {
       start = Number(req.query.s);
     }
 
-    console.log(typeof searchJSON);
-    console.log(searchJSON)
-
     Game.find(searchJSON, function(err, data) {
       if (err) return res.status(500).json({error:1});
       console.log("body", data.body);
@@ -51,43 +49,53 @@ module.exports = function(app, auth) {
 
   });
 
+
+  //add a game to user's wantgames list
   app.post('/api/games/wantsgames', auth, function(req, res){
 
     var gameId = req.body.id;
 
+    //checks to see if game ID is valid
+    Game.findById(gameId, function(err, game) {
+      if (err) return res.json({"error":'invalid id'});
+    });
+
+    //find the user based on the incoming jwt token
     User.findById(req.user._id, function(err, user) {
-      if (err) return console.log('error finding user');
-      if (user === null) return console.log('user is null');
+      if (err) return res.json({"error":'error finding user'});
+      if (user === null) return res.json({"error":'user is null'});
 
-      var alreadyWanted;
-      alreadyWanted = user.wantsGames.every(function(element, index, array) {
-        if (element == gameId) {
-          console.log('true');
-          res.json({"error": 8, "message": "game already in favorites"});
-          return true;
-        } else {
-          console.log('false');
-          return false;
+      //check to see if game is already in this user's wantsgames
+      var alreadyWanted = false;
+      for (var i = 0; i < user.wantsGames.length; i++) {
+        if (user.wantsGames[i] == gameId) {
+          alreadyWanted = true;
         }
-      });
+      }
 
+      console.log(alreadyWanted);
       if (!alreadyWanted) {
         user.wantsGames.push(gameId);
         user.save(function(err) {
-          if (err) return console.log('error saving to user hasGames');
+          if (err) return res.json({"error": 'error saving to user hasGames'});
           console.log('success');
-          res.json(user); //updated user
+          return res.json(user); //updated user
         });
       }
+      else {
+        console.log("i");
+        res.json({"error": 8, "message": "game already in favorites"});
+      }
     });
+
   });
 
   app.delete('/api/games/wantsgames', function() {
     //delete a game user no longer wants
   });
 
-
-  app.get('/api/games/mygames', auth, function(req, res) {
+  //get the hasgames and wantsgames of user
+  /*app.get('/api/games/mygames', auth, function(req, res) {
     var _id = req.user._id;
     var hasGames, wantsGames;
     User.find({"_id" : _id}, function(err, data){
@@ -102,11 +110,13 @@ module.exports = function(app, auth) {
       });
       res.status(200).send({"error":0,
         "items": {"wantsgames": wantsGames, "hasgames": hasGames}})});
-  });
+  });*/
 
+  //add a game to user's hasgames inventory
   app.post('/api/games/hasgames', auth, function(req, res){
     var newGame = new Game();
     newGame.owner = req.user._id;
+    console.log(req.body);
     newGame.title = req.body.title;
     newGame.platform =  req.body.platform;
 
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -5,16 +5,6 @@ var Game = require('../models/game');
 
 module.exports = function(app) {
 
-  //existing users come in here, generates jwt token
-  //don't have jwt token, but know password and email
-  /*app.get('/api/user', passport.authenticate('basic', {session: false}),
-    //this function overrides the default authentication
-    function(req, res) {
-      //console.log(req);
-      res.status(200).json({error: 0, jwt: req.user.generateToken(app.get('jwtSecret'))});
-    }
-  );*/
-
   app.get('/api/user', function(req, res) {//passport.authenticate('basic', {session: false}),
     var email = req.query.email;
     var password = req.query.password;
@@ -24,14 +14,13 @@ module.exports = function(app) {
     User.findOne({'email': email}, function(err, user) {
       if (err) return res.status(500).json({error: 1});
       console.log(user);
-      if (!user) return res.status(400).json({"error": 6});//done('access error');
+      if (!user) return res.status(400).json({"error": 6});
+
       //check to see if password is valid
-      if (!user.validPassword(password)) return res.status(400).json({"error": 4});//done('access error');
+      if (!user.validPassword(password)) return res.status(400).json({"error": 4});
 
       res.status(200).json({error: 0, jwt: user.generateToken(app.get('jwtSecret'))});
     });
-    //this function overrides the default authentication
-      //res.status(200).json({error: 0, jwt: req.user.generateToken(app.get('jwtSecret'))});
   });
 
   //creating a new user
@@ -65,15 +54,12 @@ module.exports = function(app) {
       newUser.zip = loc;
 
       newUser.save(function(err, data) {
-        console.log(data);
+        //console.log(data);
         if (err) return res.status(400).json({error: 1});
         res.status(200).json({error:0, jwt: newUser.generateToken(app.get('jwtSecret'))});
       });
     });
   });
-
-
-
 
   /*app.delete('/api/users', jwtadminauth, function(req, res) {
     User.findOne({'basic.email': req.body.email}, function(err, user) {
