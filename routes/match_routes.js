ec2e6f73b912cf7a21c421a4bbbc014d21c6d645
lindamariemummy <lindamariemummy@gmail.com>
2014-11-20 15:29:40 -0800

    add matching algorithm

--- routes/match_routes.js
+++ routes/match_routes.js
@@ -3,27 +3,61 @@ var Match = require('../models/match');
 var User = require('../models/user');
 var Game = require('../models/game');
 var findMatchesInDB = require('../lib/findMatchesInDB');
+var async = require('async');
 
 module.exports = function(app, auth) {
 
   app.get('/api/matches', auth, function(req, res) {
 
-    //console.log(req.user);
-    var zip = req.user.zip;
-    var matches;
+  //   //console.log(req.user);
+  //   var zip = req.user.zip;
+  //   var matches;
 
-    //find all users in the caller's zip code
-    User.find({'zip': zip}, function(err, data) {
+  //   //find all users in the caller's zip code
+  //   User.find({'zip': zip}, function(err, data) {
 
-      //add error check
+  //     //add error check
+
+  //     //run findMatches algorithm
+  //     matches = findMatchesInDB(data, res);
+
+  //      //return matches object
+  //     console.log('most recent matches', matches);
+  //     res.status(200).json({error:0, 'items': matches});
+  //     //setTimeout(function(matches){res.status(200).json({error:0, 'items': matches});}, 3000)
+
+  //   });
+
+  // });
+      var zip = req.user.zip;
+
+    async.series([
+      function(done){
+
+        var matches;
+        User.find({'zip': zip}, function(err, data) {
+
+       if (err) {console.log("hi"); done(err)}
 
       //run findMatches algorithm
-      matches = findMatchesInDB(data);
-      console.log(matches);
+          findMatchesInDB(data, res, function(err, data){
+            done(err, data);
+          });
+
        //return matches object
-      res.status(200).json({error:0, 'items': matches});
-    });
+          //console.log('most recent matches', matches);
+      //res.status(200).json({error:0, 'items': matches});
+      //setTimeout(function(matches){res.status(200).json({error:0, 'items': matches});}, 3000)
+          //done(null, matches);
+        });
 
+      }],
+      function(err, matches){
+        if (err) console.log('hi2');
+        console.log(matches[0]);
+        console.dir(matches[0][0].user1game);
+        res.status(200).json({error: 0, items: matches[0]});
+      }
+    );
   });
-
 };
