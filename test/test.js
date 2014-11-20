9c8918b029a0ef3b9ef0a88d977f4be85aa06972
lindamariemummy <lindamariemummy@gmail.com>
2014-11-19 19:29:04 -0800

    add basic test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 // grunt test runs of different database (notes_test)
-process.env.MONGO_URL = 'mongodb://localhost/game_swap_test';
+//process.env.MONGO_URL = 'mongodb://localhost/game_swap_test';
 var User = require('../models/user.js');
 var Game = require('../models/game.js');
 var chai = require('chai');
@@ -16,17 +16,17 @@ User.collection.remove(function(err) {if (err) throw err;});
 Game.collection.remove(function(err) {if (err) throw err;});
 
 describe('basic notes/users tests', function() {
-var jwt;
-var loginJSON = {email: 'test@example.com', password: 'foobar123', loc:'98027', screenName: 'foxyLinda911'};
-var loginJSONbad = {email: 'test@example.com', password: 'hi', loc:'98027', screenName: 'foxyLinda911'};
+var jwt, url = process.env.url;
+var loginURLGood = '?email=munckn'+Date.now() + '&password=Hero99999&zip=99999&screenname=crazyfool';
+var loginURLBadPW = '?email=munchkins&password=pie&zip=99999&screenname=crazyfool';
 
   it('should be able to create a new user', function(done) {
-    chai.request('http://localhost:3000')
-    .post('/api/user')
-    .send(loginJSON)
+    chai.request(url)
+    .post('api/user'+loginURLGood)
     .end(function(err, res) {
+      //console.log(res);
       expect(err).to.eql(null);
-      //expect(res.statusCode).to.eql(200);
+      expect(res.statusCode).to.eql(200);
       expect(res.body).to.have.property('jwt');
       jwt = res.body.jwt;
       expect(jwt).to.be.a('string');
@@ -35,21 +35,20 @@ var loginJSONbad = {email: 'test@example.com', password: 'hi', loc:'98027', scre
   });
 
   it('should not be able to do anything without authentication', function(done) {
-    chai.request('http://localhost:3000')
-    .get('/api/user')
+    chai.request(url)
+    .get('api/games/mygames')
     .end(function(err, res) {
       //console.log(res);
-      expect(res.statusCode).to.eql(401);
+      expect(res.statusCode).to.eql(403);
       done();
     });
   });
   it('should be able to get a token for an existing user', function(done) {
-    chai.request('http://localhost:3000')
-    .get('/api/user')
-    .auth(loginJSON.email, loginJSON.password)
+    chai.request(url)
+    .get('api/user' + loginURLGood)
     .end(function(err, res) {
       expect(err).to.eql(null);
-      //expect(res.statusCode).to.eql(200);
+      expect(res.statusCode).to.eql(200);
       expect(res.body).to.have.property('jwt');
       jwt = res.body.jwt;
       expect(jwt).to.be.a('string');
@@ -57,9 +56,8 @@ var loginJSONbad = {email: 'test@example.com', password: 'hi', loc:'98027', scre
     });
   });
   it('should refuse to create a new user with a short PW', function(done) {
-    chai.request('http://localhost:3000')
-    .post('/api/user')
-    .send(loginJSONbad)
+    chai.request(url)
+    .post('api/user' + loginURLBadPW)
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.statusCode).to.eql(400);
