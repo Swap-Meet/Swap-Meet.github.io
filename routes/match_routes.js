3979af25b1d0ed0463dadffb6db5d593d20e8012
lindamariemummy <lindamariemummy@gmail.com>
2014-11-20 15:46:21 -0800

    working basic search

--- routes/match_routes.js
+++ routes/match_routes.js
@@ -9,26 +9,6 @@ module.exports = function(app, auth) {
 
   app.get('/api/matches', auth, function(req, res) {
 
-  //   //console.log(req.user);
-  //   var zip = req.user.zip;
-  //   var matches;
-
-  //   //find all users in the caller's zip code
-  //   User.find({'zip': zip}, function(err, data) {
-
-  //     //add error check
-
-  //     //run findMatches algorithm
-  //     matches = findMatchesInDB(data, res);
-
-  //      //return matches object
-  //     console.log('most recent matches', matches);
-  //     res.status(200).json({error:0, 'items': matches});
-  //     //setTimeout(function(matches){res.status(200).json({error:0, 'items': matches});}, 3000)
-
-  //   });
-
-  // });
       var zip = req.user.zip;
 
     async.series([
@@ -37,27 +17,21 @@ module.exports = function(app, auth) {
         var matches;
         User.find({'zip': zip}, function(err, data) {
 
-       if (err) {console.log("hi"); done(err)}
-
-      //run findMatches algorithm
+       if (err) { done(err)}
+          //run findMatches algorithm
           findMatchesInDB(data, res, function(err, data){
             done(err, data);
           });
-
-       //return matches object
-          //console.log('most recent matches', matches);
-      //res.status(200).json({error:0, 'items': matches});
-      //setTimeout(function(matches){res.status(200).json({error:0, 'items': matches});}, 3000)
-          //done(null, matches);
         });
-
       }],
       function(err, matches){
         if (err) console.log('hi2');
-        console.log(matches[0]);
-        console.dir(matches[0][0].user1game);
-        res.status(200).json({error: 0, items: matches[0]});
+        //console.log(matches[0]);
+        //console.dir(matches[0][0].user1game);
+        res.status(200).json({error: 0, items: matches});
       }
     );
+
+
   });
 };
