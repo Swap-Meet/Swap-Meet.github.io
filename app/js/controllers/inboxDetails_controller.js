f1869727f273e37b3a8c10f1481f37ddc6607beb
kgentner <gentnerkarl@gmail.com>
2014-12-16 14:12:59 -0800

    fixed linter errors

--- app/js/controllers/inboxDetails_controller.js
+++ app/js/controllers/inboxDetails_controller.js
@@ -4,29 +4,30 @@
 module.exports = function(app) {
   app.controller('inboxDetailsCtrl', ['$scope', function($scope) {
 
-  //this is fake data
-  // $scope.game.title = 'Pac Man HI';
+    //this is fake data
+    // $scope.game.title = 'Pac Man HI';
 
-  $scope.game = {
-    title: 'Pac Man',
-    score: 'String',
-    publisher: 'String',
-    zip: '98087',
-    latitude: 'String',
-    longitude: 'String',
-    owner: 'String_id_number', //id number
-    short_description: 'Eat all the dots, run from the ghosts, for now...',
-    platform: 'NES',
-    image_urls: ['http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Pac_Man.svg/400px-Pac_Man.svg.png', 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']
-  };
+    $scope.game = {
+      title: 'Pac Man',
+      score: 'String',
+      publisher: 'String',
+      zip: '98087',
+      latitude: 'String',
+      longitude: 'String',
+      owner: 'String_id_number', //id number
+      short_description: 'Eat all the dots, run from the ghosts, for now...',
+      platform: 'NES',
+      image_urls: ['http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Pac_Man.svg/400px-Pac_Man.svg.png',
+      'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']
+    };
 
-  $scope.sendRequest = function() {
-    console.log('Imagine I am sending a request now...');
-  };
+    $scope.sendRequest = function() {
+      console.log('Imagine I am sending a request now...');
+    };
 
-  $scope.addFavorite = function() {
-    console.log('Imagine Adding to favorites');
-  };
+    $scope.addFavorite = function() {
+      console.log('Imagine Adding to favorites');
+    };
 
   }]);
 
--- app/js/services/game_service.js
+++ app/js/services/game_service.js
@@ -12,17 +12,20 @@ module.exports = function(app) {
               owner: '548f75df27398d8b9bfeac05',
               title: 'Monkey Island',
               platform: 'XBOX',
-              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg', 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']},
+              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg',
+              'http://res.cloudinary.com/swapmeet/image/upload/mif6vhmdjjwzo8tyg6ct.jpg']},
             { id: '548f75df27398d8b9bfeac08',
               owner: '548f75df27398d8b9bfeac05',
               title: 'Grim Fandango',
               platform: 'PC',
-              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg', 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']},
+              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg',
+              'http://res.cloudinary.com/swapmeet/image/upload/mif6vhmdjjwzo8tyg6ct.jpg']},
             { id: '548f75df27398d8b9bfeac09',
               owner: '548f75df27398d8b9bfeac05',
               title: 'Settlers of Catan',
               platform: 'Board',
-              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg', 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']}
+              image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg',
+              'http://res.cloudinary.com/swapmeet/image/upload/mif6vhmdjjwzo8tyg6ct.jpg']}
           ];
       }
     };
