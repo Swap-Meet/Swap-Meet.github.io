1445be43d3ccd5bb69b484efee3319bd4475c5ee
kgentner <gentnerkarl@gmail.com>
2014-12-16 13:54:40 -0800

    added linters to grunt-serve and fixed lint issues

--- app/js/controllers/addGame_controller.js
+++ app/js/controllers/addGame_controller.js
@@ -4,28 +4,30 @@
 module.exports = function(app) {
   app.controller('addGameCtrl', ['$scope', function($scope) {
 
-  //this is fake data
-  // $scope.game.title = 'Pac Man HI';
-  $scope.game = {
-    title: 'Pac Man',
-    score: 'String',
-    publisher: 'String',
-    zip: '98087',
-    latitude: 'String',
-    longitude: 'String',
-    owner: 'String_id_number', //id number
-    short_description: 'Eat all the dots, run from the ghosts, for now...',
-    platform: 'NES',
-    image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg', 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']
-  };
+    //this is fake data
+    // $scope.game.title = 'Pac Man HI';
+    $scope.game =
+    {
+      title: 'Pac Man',
+      score: 'String',
+      publisher: 'String',
+      zip: '98087',
+      latitude: 'String',
+      longitude: 'String',
+      owner: 'String_id_number', //id number
+      short_description: 'Eat all the dots, run from the ghosts, for now...',
+      platform: 'NES',
+      image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg',
+      'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']
+    };
 
-  $scope.addGame = function() {
-    console.log('Imagine adding a game now...');
-  };
+    $scope.addGame = function() {
+      console.log('Imagine adding a game now...');
+    };
 
-  $scope.addEditImage = function() {
-    console.log('Imagine ADD/EDIT an image here');
-  };
+    $scope.addEditImage = function() {
+      console.log('Imagine ADD/EDIT an image here');
+    };
 
   }]);
 
