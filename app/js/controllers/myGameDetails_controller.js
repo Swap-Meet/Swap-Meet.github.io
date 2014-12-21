e3094a7b0297920fa82e1b90d14fd73e4269cf2d
kgentner <gentnerkarl@gmail.com>
2014-12-21 00:33:33 -0800

    removed footer and updated myfav mygame and outbox details

--- app/js/controllers/myGameDetails_controller.js
+++ app/js/controllers/myGameDetails_controller.js
@@ -2,32 +2,18 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('myGameDetailsCtrl', ['$scope', '$location', '$http', '$cookies',
-    function($scope, $location, $http, $cookies) {
+  app.controller('myGameDetailsCtrl', ['$scope', '$location', '$http', '$cookies', '$routeParams', 'Games',
+    function($scope, $location, $http, $cookies, $routeParams, Games) {
 
       if (!$cookies.jwt) {
         $location.path('/login');
       }
       $http.defaults.headers.common['jwt'] = $cookies.jwt;
-
-    //this is fake data
-
-      $scope.game = {
-        title: 'Pac Man',
-        score: 'String',
-        publisher: 'String',
-        zip: '98087',
-        latitude: 'String',
-        longitude: 'String',
-        owner: 'String_id_number', //id number
-        short_description: 'Eat all the dots, run from the ghosts, for now...',
-        platform: 'NES',
-        image_urls: ['http://www.colinpurcell.ca/wp-content/uploads/2013/10/Pacman-02_640x250px.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Mspacmancabinet.png/512px-Mspacmancabinet.png']
-      };
+      $scope.myGames = Games.getMyList();
+      $scope.whichGame = $routeParams.indexID;
 
       $scope.removeGame = function() {
-        console.log('Imagine a game now...');
+        //should remove game from inventory
       };
 
     }]);
